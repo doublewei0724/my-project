d363fdec08dcc37da3e9278a46b32d3409c824a7
doublewei0724 <doublewei0724@gmail.com>
2026-03-05 15:31:14 +0800

    fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,21 @@
 import vue from '@vitejs/plugin-vue'
 import autoprefixer from 'autoprefixer'
-import { URL, fileURLToPath } from 'node:url'
+import path from 'path'
 import tailwindcss from 'tailwindcss'
 import { defineConfig } from 'vite'
 
-export default defineConfig(({ command }) => ({
-  base: command === 'build' ? '/my-project/' : '/',
+export default defineConfig(() => ({
+  base: '/my-project/',
   plugins: [vue()],
   resolve: {
     alias: {
       // 將 @ 指向 src 目錄
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      '@': path.resolve(__dirname, './src'),
     },
   },
   css: {
     postcss: {
       plugins: [tailwindcss(), autoprefixer()],
-    },
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        sanitizeFileName(name) {
-          return name.replace(/^_/, '')
-        },
-      },
     },
   },
 }))
